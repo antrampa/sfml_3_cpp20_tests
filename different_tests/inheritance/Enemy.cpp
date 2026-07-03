7ed82e7dacbd727661331f31236e2b24ea317df2
Antonis Zalonis <antrampa@gmail.com>
2026-07-03 22:23:05 +0200

    Refactor Enemy class to use 'lives_' instead of 'live_'

--- different_tests/inheritance/Enemy.cpp
+++ different_tests/inheritance/Enemy.cpp
@@ -2,28 +2,31 @@
 
 #include <iostream>
 
-Enemy::Enemy(std::string name, int live, int damage) : 
-        name_(name), live_(live), damage_(damage)
+Enemy::Enemy(std::string name, int lives, int damage) : 
+        name_(name), lives_(lives), damage_(damage)
 {}
 
 Enemy::Enemy(std::string name) : 
         name_(name)
 {
-    live_ = 10;
+    lives_ = 10;
     damage_ = 3;
 }
+
+Enemy::~Enemy()
+{}
 
 std::string Enemy::GetName() const 
 {
     return name_;
 }
 
-void Display() const
+void Enemy::Display() const
 {
     std::cout << "Enemy: " << name_ << ", Lives: " << lives_ << ", Damage: " << damage_ <<"\n";
 }
 
-void Attack(Enemy& target) const
+void Enemy::Attack(Enemy& target) const
 {
     std::cout << name_ <<" attacks " << target.GetName() << "!\n";
 }
