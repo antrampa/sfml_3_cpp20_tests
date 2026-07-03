4577908c6e90035a882633efb6c97760a35541bd
Antonis Zalonis <antrampa@gmail.com>
2026-07-03 21:28:21 +0200

    Add Enemy class definition in Enemy.h
    Define the Enemy class with attributes and methods.

--- different_tests/inheritance/Enemy.h
+++ different_tests/inheritance/Enemy.h
@@ -1,1 +1,34 @@
+#ifndef EXERCISE_H
+#define EXERCISE_H
+
+class Enemy 
+{
+    private:
+        std::string name_;
+        int lives_;
+        int damage_;
+    public:
+        Enemy(std::string name, int lives, int damage);
+        Enemy(std::string name);
+        ~Enemy();
+        std::string GetName() const;
+        void Display() const;
+        void Attack() const;
+}
+
+
+#endif // _EXERCISE_H
+
+
+
+
+
+
+
+
+
+
+
+
+
 
