ad9950d2c581064915248d7f45d00e7f2ee20fec
Antonis Zalonis <antrampa@gmail.com>
2026-07-03 18:21:01 +0200

    Implement Dragon class with attributes and methods

--- different_tests/classes/Dragon.cpp
+++ different_tests/classes/Dragon.cpp
@@ -2,3 +2,44 @@
 #include "Dragon.h"
 
 
+Dragon::Dragon(std::string name) : 
+        name_(name)
+{
+    health_ = 100;
+    power_ = 10;
+}
+
+Dragon::Dragon(std::string name, int health, int power) : 
+    name_(name), health_(health), power_(power)
+{}
+
+void Dragon::getName() const 
+{
+    return name_;
+}
+
+void Dragon::display() const 
+{
+    cout << "Name: " << name_ << " Health: " << health_ << "Power: " << power_ << "\n";
+}
+
+void Dragon::Attack(Dragon& targetDragon) 
+{
+    targetDragon.health_ -= power_;
+    cout << name_ << " attacks " << targetDragon.getName() << "!\n";
+}
+
+void Dragon::Heal(int amount)
+{
+    health_ += amount;
+    if(health_ > 100) 
+    {
+        health_ = 100;
+    }
+}
+
+
+void Dragon::Greet(const Dragon& dragon) const
+{
+    count << name_ << " greets " << dragon.getName() << "!\n";
+}
