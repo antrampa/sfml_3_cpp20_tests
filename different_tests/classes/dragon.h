2620a81868d5b99a23f8bfdbcbbd340eac507e09
Antonis Zalonis <antrampa@gmail.com>
2026-07-03 18:08:49 +0200

    Add Dragon class with basic attributes and methods

---
+++ different_tests/classes/dragon.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Dragon 
+{
+    private:
+        std::string name_;
+        int health_;
+        int power_;
+        
+    public:
+        Dragon(std::string name)
+        Dragon(std::string name, int health, int power);
+        ~Dragon();
+        void display() const;
+        void Attack(Dragon& dragon);
+        void Heal(int amount);
+        void Greet(const Dragon& dragon) const;
+}
